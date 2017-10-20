775053ac57d4921814c71716f4b7bbe9e4f35193
infarot <dawidweglinski123@gmail.com>
2017-10-20 19:14:54 +0200

    Archival - podstawy

---
+++ SGGWSupportWeb/Controllers/ArchivalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGGWSupportWeb.Controllers
+{
+    public class ArchivalController : Controller
+    {
+        // GET: Archival
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SGGWSupportWeb/Models/ArchivalViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SGGWSupportWeb.Models
+{
+    public class ArchivalViewModel
+    {
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Numer zgłoszenia")]
+        public string IncidentNo { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Przypisano do")]
+        public string AssignTo { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Kategoria")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Wnioskodawca")]
+        public string Requestor { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Priorytet")]
+        public string Priority { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [Display(Name = "Tytuł")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Data wykonania")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime ResolvedDate { get; set; }
+    }
+}
