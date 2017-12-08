f02521f3826e66c9fb15265738a8e0ee36ccddc8
kmakowski21 <kamil.mak@wp.pl>
2017-12-08 23:16:46 +0100

    Pobieranie zgłoszeń poprzez WebService

--- SGGWSupportWeb/Controllers/TicketController.cs
+++ SGGWSupportWeb/Controllers/TicketController.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using SGGWSupportWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace SGGWSupportWeb.Controllers
 {
     public class TicketController : Controller
     {
+        string Baseurl = "http://webservice.adscan.pl:8090/";
+
         // GET: Ticket
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            List<TicketViewModel> tickets = new List<TicketViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage message = await client.GetAsync("tasks");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var ticketsResponse = message.Content.ReadAsStringAsync().Result;
+                    tickets = JsonConvert.DeserializeObject<List<TicketViewModel>>(ticketsResponse);
+                }
+            }
+
+                return View(tickets);
         }
 
         public ActionResult AddTicket()
@@ -33,6 +57,8 @@ namespace SGGWSupportWeb.Controllers
         {
             return View();
         }
+
+        
 
     }
 }
