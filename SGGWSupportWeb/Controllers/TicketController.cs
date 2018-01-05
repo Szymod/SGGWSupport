06dd4e0df17ca721487dbcb057e0029166b9c7a4
kmakowski21 <kamil.mak@wp.pl>
2018-01-06 00:27:22 +0100

    Zgłoszenia c.d.

--- SGGWSupportWeb/Controllers/TicketController.cs
+++ SGGWSupportWeb/Controllers/TicketController.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
+using SGGWSupportWeb.Extensions;
+using System.Net.Http.Formatting;
 
 namespace SGGWSupportWeb.Controllers
 {
@@ -38,9 +40,76 @@ namespace SGGWSupportWeb.Controllers
                 return View(tickets);
         }
 
-        public ActionResult AddTicket()
+        public async Task<ActionResult> AddTicket()
         {
+            List<CategoryViewModel> categories = new List<CategoryViewModel>();
+            List<PriorityViewModel> priorities = new List<PriorityViewModel>();
+            List<SelectListItem> categoryList = new List<SelectListItem>();
+            List<SelectListItem> priorityList = new List<SelectListItem>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-AUTH-TOKEN", Session.GetToken().Token);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage message = await client.GetAsync("category");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var categoryResponse = message.Content.ReadAsStringAsync().Result;
+                    categories = JsonConvert.DeserializeObject<List<CategoryViewModel>>(categoryResponse);
+                }
+
+                message = await client.GetAsync("priority");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var priorityResponse = message.Content.ReadAsStringAsync().Result;
+                    priorities = JsonConvert.DeserializeObject<List<PriorityViewModel>>(priorityResponse);
+                }
+
+                foreach (var el in categories)
+                {
+                    categoryList.Add(new SelectListItem { Text = el.Name, Value = (el.id.ToString()) });
+                }
+
+                foreach (var el in priorities)
+                {
+                    priorityList.Add(new SelectListItem { Text = el.Name, Value = (el.id.ToString()) });
+                }
+            }
+
+            ViewData["categories"] = categoryList;
+            ViewData["priorities"] = priorityList;
+
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddTicket(TicketViewModel ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-AUTH-TOKEN", Session.GetToken().Token);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                HttpResponseMessage message = await client.PostAsJsonAsync("tasks", ticket);
+                message.EnsureSuccessStatusCode();
+
+                ModelState.Clear();
+
+                return RedirectToAction("Index", "Ticket");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Ticket");
+            }
         }
 
         public ActionResult EditTicket()
---
+++ SGGWSupportWeb/Models/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGGWSupportWeb.Models
+{
+    public class CategoryViewModel
+    {
+        public int id { set; get; }
+        public string Name { set; get; }
+    }
+}
