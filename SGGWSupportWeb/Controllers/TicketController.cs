4fd1dd17b25827a355bec1b8961933e1e6c1c6f4
kmakowski21 <kamil.mak@wp.pl>
2018-01-10 22:01:14 +0100

    Metoda EditTicket

--- SGGWSupportWeb/Controllers/TicketController.cs
+++ SGGWSupportWeb/Controllers/TicketController.cs
@@ -46,7 +46,7 @@ namespace SGGWSupportWeb.Controllers
             List<StateViewModel> states = new List<StateViewModel>(); 
             List<SelectListItem> categoryList = new List<SelectListItem>();
             List<SelectListItem> priorityList = new List<SelectListItem>();
-            List<SelectListItem>  statesList = new List<SelectListItem>();
+            List<SelectListItem> statesList = new List<SelectListItem>();
 
             using (var client = new HttpClient())
             {
@@ -132,9 +132,112 @@ namespace SGGWSupportWeb.Controllers
             }
         }
 
-        public ActionResult EditTicket()
+        public async Task<ActionResult> EditTicket(int id)
         {
-            return View();
+            TicketViewModel ticket = new TicketViewModel();
+
+            List<CategoryViewModel> categories = new List<CategoryViewModel>();
+            List<PriorityViewModel> priorities = new List<PriorityViewModel>();
+            List<StateViewModel> states = new List<StateViewModel>();
+            List<SelectListItem> categoryList = new List<SelectListItem>();
+            List<SelectListItem> priorityList = new List<SelectListItem>();
+            List<SelectListItem> statesList = new List<SelectListItem>();
+
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Add("X-AUTH-TOKEN", Session.GetToken().Token);
+
+                HttpResponseMessage message = await client.GetAsync("category");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var categoryResponse = message.Content.ReadAsStringAsync().Result;
+                    categories = JsonConvert.DeserializeObject<List<CategoryViewModel>>(categoryResponse);
+                }
+
+                message = await client.GetAsync("priority");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var priorityResponse = message.Content.ReadAsStringAsync().Result;
+                    priorities = JsonConvert.DeserializeObject<List<PriorityViewModel>>(priorityResponse);
+                }
+
+                message = await client.GetAsync("statuses");
+
+                if (message.IsSuccessStatusCode)
+                {
+                    var statesResponse = message.Content.ReadAsStringAsync().Result;
+                    states = JsonConvert.DeserializeObject<List<StateViewModel>>(statesResponse);
+                }
+
+                foreach (var el in categories)
+                {
+                    categoryList.Add(new SelectListItem { Text = el.Name, Value = (el.Id.ToString()) });
+                }
+
+                foreach (var el in priorities)
+                {
+                    priorityList.Add(new SelectListItem { Text = el.Name, Value = (el.Id.ToString()) });
+                }
+
+                foreach (var el in states)
+                {
+                    statesList.Add(new SelectListItem { Text = el.Name, Value = (el.Id.ToString()) });
+                }
+            
+
+                message = await client.GetAsync("tasks/" + id);
+
+
+
+                    if (message.IsSuccessStatusCode)
+                    {
+                        var ticketResponse = message.Content.ReadAsStringAsync().Result;
+                        ticket = JsonConvert.DeserializeObject<TicketViewModel>(ticketResponse);
+                    }
+                
+                }
+
+                ViewBag.Id = id.ToString();
+                ViewData["states"] = statesList;
+                ViewData["categories"] = categoryList;
+                ViewData["priorities"] = priorityList;
+
+                return View(ticket);
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditTicket(TicketViewModel ticket)
+        {
+            ModelState["Status.Name"].Errors.Clear();
+            ModelState["Priority.Name"].Errors.Clear();
+            ModelState["Category.Name"].Errors.Clear();
+
+            if (ModelState.IsValid)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(Baseurl);
+                    client.DefaultRequestHeaders.Add("X-AUTH-TOKEN", Session.GetToken().Token);
+
+                    ticket.UserData = Session.GetCurrentUser();
+                    ticket.TicketAddTime = DateTime.Now;
+
+                    HttpResponseMessage message = await client.PostAsJsonAsync("tasks", ticket);
+                    message.EnsureSuccessStatusCode();
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
 
         public ActionResult DeleteTicket()
